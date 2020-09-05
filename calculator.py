dde33797484ec089123d9d00209f4926ee265fb9
ganesh <ganesh.kolavennu@gmail.com>
2020-09-05 16:00:15 +0530

    Added a new feature !

--- calculator.py
+++ calculator.py
@@ -3,15 +3,24 @@ from subtract import *
 from multiply import *
 from divide import *
 
-a = int(input("Enter first number : "))
-b = int(input("Enter second number : "))
-operator = input("Enter + or - or * or / : ")
+decision = True
 
-if operator == "+":
-    addition(a, b)
-elif operator == "-":
-    subtraction(a, b)
-elif operator == "*":
-    multiplication(a, b)
-elif operator == "/":
-    division(a, b)
+while decision:
+    a = int(input("Enter first number : "))
+    b = int(input("Enter second number : "))
+    operator = input("Enter + or - or * or / : ")
+
+    if operator == "+":
+        addition(a, b)
+    elif operator == "-":
+        subtraction(a, b)
+    elif operator == "*":
+        multiplication(a, b)
+    elif operator == "/":
+        division(a, b)
+
+    user_inp = input("Would you like to continue [Y/N] : ")
+    if user_inp == "Y":
+        decision
+    else:
+        break
